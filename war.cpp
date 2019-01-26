08ea8724988ecf5f7f021cec00200aa415f0ad8e
David Rozmajzl <dmr121@zips.uakron.edu>
2019-01-26 11:12:21 -0500

    Create and test Rank and Suit enums

--- war.cpp
+++ war.cpp
@@ -1,8 +1,35 @@
+#include <iostream>
+
+enum Suit {
+	Heart,
+	Diamond,
+	Club,
+	Spade
+};
+
+enum Rank {
+	Two,
+	Three,
+	Four,
+	Five,
+	Six,
+	Seven,
+	Eight,
+	Nine,
+	Ten,
+	Jack,
+	Queen,
+	King,
+	Ace
+
+};
+
 struct Card {
 	Suit suit;
 	Rank rank;
-}
+};
 
 int main() {
-	return 0;
+	Card test{Heart, Nine};
+	std::cout << test.suit;
 }
